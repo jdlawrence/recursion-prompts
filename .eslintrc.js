cc9c7ad93a77705b40b3ccd10d5e5826ded07758
jdlawrence <jamil.lawrence@gmail.com>
2017-03-13 21:55:08 -0700

    Added react rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,12 +6,29 @@ module.exports = {
         "import"
     ],
     'rules': {
+      'no-mixed-operators': 0,
       'no-var': 0,
       'vars-on-top': 0,
       'no-unused-vars': 0,
       'func-names': 0,
       'no-else-return': 0,
       'no-console': 0,
-      'no-param-reassign': 0
+      'no-param-reassign': 0,
+      'react/jsx-wrap-multilines': 0,
+      'react/no-unused-prop-types': 0,
+      'react/jsx-first-prop-new-line':0,
+      'react/style-prop-object': 0,
+      'react/jsx-no-target-blank': 0,
+      'react/jsx-filename-extension': 0,
+      'react/jsx-no-comment-textnodes': 0,
+      'react/no-children-prop':0,
+      'react/no-render-return-value':0,
+      'react/jsx-tag-spacing':0,
+      'react/require-render-return':0,
+      'react/no-find-dom-node':0,
+      'react/no-danger-with-children':0,
+      'react/no-unescaped-entities':0,
+      'react/no-array-index-key':0,
+      'react/require-default-props':0,
     }
 };
